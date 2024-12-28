1edfa952f5a443988cfbba01bb327ada94e87e84
keerthi-undavalli <keerthiundavalli12@gmail.com>
2024-12-28 14:28:37 +0530

    Add files via upload

---
+++ slice5.py
@@ -0,0 +1,11 @@
+#creation of list
+prog_lang=['C','C++','Java','Python','Flutter','Cobalt','Dart','Javascript']
+print(prog_lang)
+print(type(prog_lang))
+print("------------------------------------------------------------------------")
+#slicing
+#extract a part of list or divide the list into sub lists
+print("slicing of lists:")
+print(prog_lang[:4])
+print(prog_lang[4:])
+print(prog_lang[::2])
